23148fd4b553435849fd82dc9a85c5e52d0d442c
Rubylnic <ksenia.bolshako@mail.ru>
2019-09-03 20:47:36 +0800

    Защита

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del")
 
-gulp.task("css", function () {
+gulp.task("css", function() {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
@@ -16,14 +19,32 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+      "source/fonts/**/*.{woff, woff2}",
+      "source/img/**",
+      "source/js/**",
+      "source/*.html"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("server", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -34,4 +55,5 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+gulp.task("build", gulp.series("clean", "copy", "css"));
 gulp.task("start", gulp.series("css", "server"));
